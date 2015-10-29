f05b50c8a97926d7c6556ce75510a26dfe09e88f
uyab <uyab.exe@gmail.com>
2015-10-29 22:31:44 +0700

    fix root user seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,13 +14,19 @@ class DatabaseSeeder extends Seeder
     {
         Model::unguard();
 
+        // root role
+        $root = \Laravolt\Acl\Models\Role::create(['name' => 'root']);
+
         // root user
-        $root = factory(\App\Entities\User::class)->create(['email' => 'root@laravolt.com', 'status' => \App\Enum\UserStatus::ACTIVE()]);
-        $root->assignRole('root');
+        $user = factory(\App\Entities\User::class)->create(['email' => 'root@laravolt.com', 'status' => \App\Enum\UserStatus::ACTIVE()]);
+        $user->assignRole($root);
 
         factory(\App\Entities\User::class, 100)->create();
 
-        factory(\App\Entities\Post::class, 10)->create();
+        factory(\App\Entities\Post::class, 10)->make()->each(function($post){
+            $post->setResponsibleUser(\App\Entities\User::first());
+            $post->save();
+        });
 
         Model::reguard();
     }
