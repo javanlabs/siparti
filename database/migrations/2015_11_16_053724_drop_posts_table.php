81445cd28e4637eeabfa9c5b7359fa82bea63b18
uyab <uyab.exe@gmail.com>
2015-11-16 12:41:36 +0700

    hapus table dan seeder post

---
+++ database/migrations/2015_11_16_053724_drop_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DropPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->drop();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('author_id');
+            $table->string('title');
+            $table->text('content');
+            $table->timestamps();
+        });
+    }
+}
