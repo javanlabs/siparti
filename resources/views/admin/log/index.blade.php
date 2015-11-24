017a46c338300cd6ba2052758fb949f07f266091
akiu <ricky3andika@gmail.com>
2015-11-24 12:29:21 +0700

    remove delete button log

--- resources/views/admin/log/index.blade.php
+++ resources/views/admin/log/index.blade.php
@@ -29,19 +29,16 @@
                 <table class="ui very compact table bottom small sortable">
                     <thead>
                     <tr>
-                        <th ><button data-click-state="0" id="checkAll"><i class="check circle icon icon-button"></i></button></th>
                         <th>Tanggal</th>
                         <th>Ip address</th>
                         <th>User</th>
                         <th>Email address</th>
                         <th>Activity</th>
-             			<th>&nbsp;</th>
                     </tr>
                     </thead>
                     <tbody>
                     @forelse($logs as $data)
                         <tr>
-                            <td><input class="deletedId" type="checkbox" value="{!! $data->present('id') !!}" /></td>
                             <td>{!! $data->present('date_for_human') !!}</td>
                             <td>{!! $data->present('ip_address') !!}</td>
                             <td>{!! $data->present('name') !!}</td>
@@ -49,15 +46,7 @@
                             
                             	
                             <td>{!! $data->present('activity') !!}</td>
-                       		<td class="right aligned">
-                              	
-                              	<form role="form" action="{{ route('admin.logs.destroy',  [ 'id' => $data->present('id') ]) }}" method="POST" id="delete-form">
-                                	<input type="hidden" name="_method" value="DELETE">
-                                	{{ csrf_field() }}
-                              	</form>
-
-                              	<button class="ui red button basic mini delete-button"><i class="large remove icon"></i></button>
-                            </td>
+                       		
 
                         </tr>
 
@@ -67,13 +56,7 @@
                         </tr>
                     @endforelse
                   </tbody>
-                  <tfoot>
-                    	<tr>
-                    		<td>
-                    			<button class="negative ui button" id="deleteMultiple">X</button>
-                    		</td>
-                    	</tr>
-                    </tfoot>
+                
                 </table>
             </div>
             <div class="ui menu bottom attached">
@@ -84,48 +67,4 @@
             </div>
         </section>
     </div>
-
-    <div class="ui small test modal deleteConfirm">
-      <div class="header">
-        Hapus Log
-      </div>
-    <div class="content">
-      <p>Apakah anda akan menghapus log ini ?</p>
-    </div>
-    <div class="actions">
-        <div class="ui negative button">
-        Tidak
-        </div>
-        <div class="ui positive right labeled icon button yess-button">
-          Ya
-          <i class="checkmark icon"></i>
-        </div>
-      </div>
-    </div>
-
-    <div class="ui small test modal multipleDeleteConfirm">
-      <div class="header">
-        Hapus Log
-      </div>
-    <div class="content">
-      <p>Anda akan menghapus semua log yang telah dicentang ?</p>
-    </div>
-    <div class="actions">
-        <div class="ui negative button">
-        Tidak
-        </div>
-        <div class="ui positive right labeled icon button yess2-button">
-          Ya
-          <i class="checkmark icon"></i>
-        </div>
-      </div>
-    </div>
-
-    <form method="post" action="{{ route('admin.logs.deleteMultiple') }}" role="form" id="multipleDeletedForm">
-      {{ csrf_field() }}
-
-    </form>
-
-
-
 @endsection
