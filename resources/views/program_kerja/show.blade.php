18c64d3b87149eaadf849f3e27aa4f626bd7e951
uyab <uyab.exe@gmail.com>
2015-11-26 10:10:24 +0700

    dummy timeline

--- resources/views/program_kerja/show.blade.php
+++ resources/views/program_kerja/show.blade.php
@@ -2,44 +2,58 @@
 
 @section('content')
     <section class="ui container page">
-        <div class="ui grid">
-            <div class="column sixteen wide">
-                <h2 class="">{!! $programKerja->present('name') !!}</h2>
+
+        <h2 class="">{!! $programKerja->present('name') !!}</h2>
+
+        <div class="ui celled grid relaxed" style="background: #ffffff">
+            <div class="row">
+                <div class="ten wide column">
+                    <table class="ui definition table small">
+                        <tbody>
+                        <tr><td style="width: 150px">Fase Sekarang</td><td>{!! $programKerja->present('label') !!}</td></tr>
+                        <tr><td>Periode</td><td>{{ $programKerja->present('periode') }}</td></tr>
+                        <tr><td>Instansi Terkait</td><td>{{ $programKerja->present('instansi_terkait') }}</td></tr>
+                        <tr><td>Deskripsi</td><td>{{ $programKerja->present('description') }}</td></tr>
+                        <tr>
+                            <td>Fase Terkait</td>
+                            <td>
+                                <div class="ui list divided middle aligned relaxed">
+                                    @foreach($related as $item)
+                                        <div class="item">
+                                            {!! $item->present('label') !!}
+                                            <a href="{{ $item->present('url') }}">{{ $item->present('periode') }}</a>
+                                        </div>
+                                    @endforeach
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Dokumen</td>
+                            <td>
+                                <div class="ui list divided middle aligned relaxed">
+                                    @foreach($documents as $item)
+                                        <div class="item">
+                                            <a href="{{ $item->getUrl() }}">{{ $item->file_name }}</a>
+                                        </div>
+                                    @endforeach
+                                </div>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="six wide column" style="background: #fafafa">
+                    <div class="ui divider hidden"></div>
+                    <h3 class="ui header">Timeline</h3>
+                    <div class="ui divider hidden"></div>
+                    <div class="ui divider hidden"></div>
+                    @include('program_kerja.timeline')
+                </div>
             </div>
+        </div>
+
+        <div class="ui grid">
             <div class="column ten wide">
-                <table class="ui definition table small">
-                    <tbody>
-                    <tr><td style="width: 150px">Fase Sekarang</td><td>{!! $programKerja->present('label') !!}</td></tr>
-                    <tr><td>Periode</td><td>{{ $programKerja->present('periode') }}</td></tr>
-                    <tr><td>Instansi Terkait</td><td>{{ $programKerja->present('instansi_terkait') }}</td></tr>
-                    <tr><td>Deskripsi</td><td>{{ $programKerja->present('description') }}</td></tr>
-                    <tr>
-                        <td>Fase Terkait</td>
-                        <td>
-                            <div class="ui list divided middle aligned relaxed">
-                                @foreach($related as $item)
-                                    <div class="item">
-                                        {!! $item->present('label') !!}
-                                        <a href="{{ $item->present('url') }}">{{ $item->present('periode') }}</a>
-                                    </div>
-                                @endforeach
-                            </div>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Dokumen</td>
-                        <td>
-                            <div class="ui list divided middle aligned relaxed">
-                                @foreach($documents as $item)
-                                    <div class="item">
-                                        <a href="{{ $item->getUrl() }}">{{ $item->file_name }}</a>
-                                    </div>
-                                @endforeach
-                            </div>
-                        </td>
-                    </tr>
-                    </tbody>
-                </table>
                 <div class="ui segment very padded">
                 @include('elements.share')
                     <p>
---
+++ resources/views/program_kerja/timeline.blade.php
@@ -0,0 +1,78 @@
+<div class="ui feed timeline">
+    <div class="event">
+        <div class="label">
+            <span class="ui empty circular label"></span>
+        </div>
+
+        <div class="content">
+            <div class="date">
+                1 Januari 2010
+            </div>
+            <div class="summary">
+                <a class="user">
+                    Elliot Fu
+                </a> mengusulkan program kerja:
+            </div>
+            <a class="extra text">
+                Judul usulan program kerja
+            </a>
+        </div>
+    </div>
+    <div class="event">
+        <div class="label">
+            <span class="ui empty circular label"></span>
+        </div>
+
+        <div class="content">
+            <div class="date">
+                1 Januari 2010
+            </div>
+            <div class="summary">
+                <a class="user">
+                    Elliot Fu
+                </a> mengusulkan program kerja:
+            </div>
+            <a class="extra text">
+                Judul usulan program kerja
+            </a>
+        </div>
+    </div>
+    <div class="event">
+        <div class="label">
+            <span class="ui empty circular label"></span>
+        </div>
+
+        <div class="content">
+            <div class="date">
+                14 Januari 2010
+            </div>
+            <div class="summary">
+                Fase <span class="ui basic mini label teal ">PERENCANAAN</span>
+            </div>
+            <a class="extra text">
+                <small>
+                    (Excerpt) Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet blanditiis et expedita hic iure laborum magni nobis non nulla porro possimus quisquam recusandae reiciendis sed velit veniam, voluptate! Incidunt, nobis.
+                </small>
+            </a>
+        </div>
+    </div>
+    <div class="event">
+        <div class="label">
+            <span class="ui empty circular label"></span>
+        </div>
+
+        <div class="content">
+            <div class="date">
+                20 Maret 2010
+            </div>
+            <div class="summary">
+                Fase <span class="ui basic mini label green ">PELAKSANAAN</span>
+            </div>
+            <a class="extra text">
+                <small>
+                    (Excerpt) Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet blanditiis et expedita hic iure laborum magni nobis non nulla porro possimus quisquam recusandae reiciendis sed velit veniam, voluptate! Incidunt, nobis.
+                </small>
+            </a>
+        </div>
+    </div>
+</div>
