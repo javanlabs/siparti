c604997077e6ccdb2a74d4ed8880675497d17146
akiu <ricky3andika@gmail.com>
2015-11-26 14:01:58 +0700

    edit form program kerja, penambahan pilihan usulan

--- resources/views/admin/programKerja/form.blade.php
+++ resources/views/admin/programKerja/form.blade.php
@@ -112,7 +112,7 @@
                         <br />
                         {!! SemanticForm::submit('Simpan') !!}
 
-                    @else
+                        @else
 
                         <br />
                     
@@ -178,9 +178,57 @@
                     
                 </div>
             </div>
+            <div class="six wide column">
+               <div class="ui segment">
+                    <h3 class="ui header">Program Kerja Terkait</h3>
+                    
+                    <div class="ui form">
+                        <div class="fields inline">
+                            <select id="optionProgramKerja" class="ui dropdown fluid search selection">
+
+                                <?php $arrayId = []; ?>
+              
+                                <?php $arrayName = []; ?>
+              
+                                @foreach ($usulan as $data)
+                                    {!! $arrayId[] = $data->present('id') !!}
+                                    {!! $arrayName[] = $data->present('name') !!}  
+                                @endforeach
+
+                                @foreach ($usulan as $data)
+                                    @if (!in_array($data->present('id'), $arrayId) and !in_array($data->present('name'), $arrayName))
+                                    <option value="{{ $data->present('id') }}">{{ $data->present('name') }}</option>
+                                    @endif
+                                @endforeach
+
+                            </select>
+                            &nbsp;
+                            <button id="addProgramKerja" class="ui button icon green"><i class="icon plus"></i></button>
+                        </div>
+                    </div>
+
+                    <div class="ui divider"></div>
+
+                    <div id="programKerjaContainer" class="ui middle aligned divided list relaxed">
+                        
+                        @foreach($usulan as $data)
+              
+                        <div class="item">
+                            <div class="right floated content">
+                                <div data-text="{{ $data->name }}" data-val="{{ $data->id }}" class="ui button mini hapus">Hapus</div>
+                            </div>
+                            <div class="content">{{ $data->name }}</div>
+                        </div>
+            
+                        @endforeach
+                    
+                    </div>
+
+                </div>
+            </div>
         </div>
     </section>      
-    </div>
+</div>
     
 @endsection
 
