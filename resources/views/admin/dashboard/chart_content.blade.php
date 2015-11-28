ef56ffdbc8693736111f6370df105447f464df4b
akiu <ricky3andika@gmail.com>
2015-11-28 16:37:13 +0700

    ganti ukuran chart

--- resources/views/admin/dashboard/chart_content.blade.php
+++ resources/views/admin/dashboard/chart_content.blade.php
@@ -7,7 +7,7 @@
     <div class="ui grid">
         <div class="thirteen wide column">
             <div>
-                <canvas id="canvas" width="700" height="500"></canvas>
+                <canvas id="canvas" width="700" height="250"></canvas>
             </div>
         </div>
         <div class="three wide column">
--- resources/views/admin/dashboard/chart_interaction.blade.php
+++ resources/views/admin/dashboard/chart_interaction.blade.php
@@ -4,7 +4,7 @@
     <div class="ui grid">
         <div class="thirteen wide column">
             <div>
-                <canvas id="canvas2" width="700" height="500"></canvas>
+                <canvas id="canvas2" width="700" height="250"></canvas>
             </div>
         </div>
         <div class="three wide column">
