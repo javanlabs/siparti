f5f5e213b549ceb2791d5f636b1ac371f258d0ae
akiu <ricky3andika@gmail.com>
2015-12-04 15:52:22 +0700

    handle old input create fase program kerja

--- resources/views/admin/faseProgramKerja/form.blade.php
+++ resources/views/admin/faseProgramKerja/form.blade.php
@@ -10,13 +10,12 @@
     <script>
           $(document).ready(function() {
 
-            $("#textRedactor").redactor({
+          $("#textRedactor").redactor({
                 minHeight: 300,
                 imageUpload: '/image/upload',
                 plugins : ['imageManager'],
                 buttons: ['html', 'bold', 'italic', 'deleted', 'link', 'unorderedlist', 'orderedlist', 
                 'outdent', 'indent', 'image', 'link', 'alignment', 'horizontalrule'],
-                
                 modalClosedCallback : function()
                 {
 
@@ -29,25 +28,41 @@
                   alert("Tidak bisa upload gambar, periksa koneksi internet")
                 }
             });
-                
-            var textTentang = $("#descriptionText").text();
+            
+            @if($action == "edit")
+
+                var textTentang = $("#descriptionText").text();
         
-            $(".redactor-editor").text("");   
+                $(".redactor-editor").text("");   
                 
-            $("#textRedactor").redactor('code.set', textTentang);
+                $("#textRedactor").redactor('code.set', textTentang);
+
+            @else
+
+              var text = $("#oldText").html();
+
+              $("#textRedactor").redactor("code.set", text);
+
+            @endif
+            
 
             $('.tagMultiple.ui.fluid.multiple.search.selection.dropdown')
               .dropdown({
                   allowAdditions: true
               });
           });
-       
     </script>
     
 @endsection
 @section('content')
+  
+  <div id="oldText">
+      
+      {!! Input::old('description') !!}
 
-	<div class="ui container">
+  </div>
+	
+  <div class="ui container">
 	<section class="ui container page" id="page-fase-program-kerja-create">
         <div class="ui centered grid">
             <div class="ten wide column">
@@ -64,7 +79,7 @@
                       	<div class="field">
                       	    <label>Nama Program Kerja</label>
                       		  <div class="ui fluid search selection dropdown">
-  							               <input name="proker_id" type="hidden" value="16">
+  							               <input name="proker_id" type="hidden" value="{{ Input::old('proker_id') }}">
   							               <i class="dropdown icon"></i>
   							               <div class="default text">Nama Program Kerja</div>
   								                <div class="menu">
@@ -78,7 +93,7 @@
                         <div class="field">
                       	    <label>Fase</label>
                       		  <div class="ui selection dropdown">
-  								              <input name="type" type="hidden">
+  								              <input name="type" type="hidden" value="{{ Input::old('type') }}">
   								              <i class="dropdown icon"></i>
   								              <div class="default text">Fase</div>
   								              <div class="menu">
@@ -92,7 +107,7 @@
                         <div class="field">
                       	     <label>Satuan Kerja</label>
                       		    <div class="ui fluid search selection dropdown">
-  							                 <input name="satker_id" type="hidden">
+  							                 <input name="satker_id" type="hidden" value="{{ Input::old('satker_id') }}">
   							                 <i class="dropdown icon"></i>
   							                 <div class="default text">Satuan Kerja</div>
   								               <div class="menu">
@@ -105,22 +120,37 @@
                         
                         <div class="field">
                         	<label>Scope</label>
-                        	<input name="scope" type="text"  />
+                        	<input name="scope" type="text"  value="{{ Input::old('scope') }}"/>
                         </div>
                         
-                        {!! SemanticForm::text('target', 'Target') !!}
-                        {!! SemanticForm::text('progress', 'Progress') !!}
-                       	{!! SemanticForm::text('kendala', 'Kendala') !!}
-                        {!! SemanticForm::text('instansi_terkait', 'Instansi Terkait') !!}
-                        
+                        <div class="field">
+                          <label>Target</label>
+                          <input name="target" type="text" value="{{ Input::old('target') }}"  />
+                        </div>
+
+                        <div class="field">
+                          <label>Progress</label>
+                          <input name="progress" type="text"  value="{{ Input::old('progress') }}"/>
+                        </div>
+
+                        <div class="field">
+                          <label>Kendala</label>
+                          <input name="kendala" type="text"  value="{{ Input::old('kendala') }}"/>
+                        </div>
+
+                        <div class="field">
+                          <label>Instansi Terkait</label>
+                          <input name="instansi_terkait" type="text" value="{{ input::old('instansi_terkait') }}"  />
+                        </div>
+
                         <div class="field">
                         	<label>Tanggal Mulai</label>
-                        	<input name="start_date" type="text" class="dateInput" data-beatpicker-module="icon,today,clear" data-beatpicker="true" />
+                        	<input value="{{ Input::old('start_date') }}" name="start_date" type="text" class="dateInput" data-beatpicker-module="icon,today,clear" data-beatpicker="true" />
                         </div>
                        	
                         <div class="field">
                         	<label>Tanggal Selesai</label>
-                        	<input name="end_date" type="text" class="dateInput" data-beatpicker-module="icon,today,clear" data-beatpicker="true" />
+                        	<input value="{{ Input::old('end_date') }}" name="end_date" type="text" class="dateInput" data-beatpicker-module="icon,today,clear" data-beatpicker="true" />
                         </div>
                         
                         <div class="field">
@@ -128,10 +158,17 @@
                         	<textarea rows="50" id="textRedactor" name="description"></textarea>
                         </div>
 
-                        {!! SemanticForm::text('pic', 'Pic') !!}
-  						          {!! SemanticForm::text('pagu', 'Pagu') !!}
-  						
-  						          <div class="field">
+                        <div class="field">
+                          <label>Pic</label>
+                          <input name="pic" type="text" value="{{ input::old('pic') }}"  />
+                        </div>
+
+                        <div class="field">
+                          <label>Pagu</label>
+                          <input name="pagu" type="text" value="{{ input::old('pagu') }}"  />
+                        </div>
+
+                        <div class="field">
                             <label for="">Dokumen Terkait</label>
                             <input type="file" name="file[]"><br>
                             <input type="file" name="file[]"><br>
@@ -167,7 +204,7 @@
                         <div class="field">
                           <label>Tags</label>
                           <div id="tagsMultiple" class="tagMultiple ui fluid multiple search selection dropdown">
-                            <input name="tags" type="hidden">
+                            <input name="tags" type="hidden" value="{{ Input::old('tags') }}" >
                             <i class="dropdown icon"></i>
                             <div class="default text">Tags</div>
                              <div class="menu">
