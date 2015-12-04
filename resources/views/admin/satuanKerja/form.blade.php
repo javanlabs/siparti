e0050d72b800bc8b5d6e26192933dc5ad9446c7e
akmalfdy <akmal.fuady@outlook.com>
2015-12-04 17:48:18 +0700

    fixing indentation in category and satker

--- resources/views/admin/satuanKerja/form.blade.php
+++ resources/views/admin/satuanKerja/form.blade.php
@@ -8,43 +8,36 @@
 @endsection
 @section('content')
 
-	<div class="ui container">
-	<section class="ui container page" id="page-program-kerja-usulan-create">
+<div class="ui container">
+    <section class="ui container page" id="page-program-kerja-usulan-create">
         <div class="ui centered grid">
             <div class="ten wide column">
                 <div class="ui segment very padded">
                 	@if ($action == "create")
-                    <h2 class="ui header text centered"><span>Membuat</span> Satuan Kerja</h2>
-        					@else
-        					<h2 class="ui header text centered"><span>Edit</span> Satuan Kerja</h2>
-        					@endif
-
-                  <form class="ui form" action="{{ $route }}" class="ui form large" method="POST" enctype="multipart/form-data">
-                  @if ($action == "create")    
-                      {{ csrf_field() }}
-
-                      {!! SemanticForm::text('name', 'Nama Satuan Kerja', old('name')) !!}
-
-                    	{!! SemanticForm::submit('Simpan') !!}
-                  @else
-                  	{{ csrf_field() }}
-                      <input type="hidden" name="_method" value="PUT">
-                    
-                      <div class="field">
-                      	<label>Nama Satuan Kerja</label>
-                      	<input name="name" value="{{ (old('name')) ? old('name') : $satker->present('name') }}">
-                      </div>
-       				                        
-                   	{!! SemanticForm::submit('Simpan') !!}
-                     	
-                  @endif   	
-                  </form>
-                  
+                        <h2 class="ui header text centered"><span>Membuat</span> Satuan Kerja</h2>
+					@else
+					   <h2 class="ui header text centered"><span>Edit</span> Satuan Kerja</h2>
+					@endif
+                    <form class="ui form" action="{{ $route }}" class="ui form large" method="POST" enctype="multipart/form-data">
+                        @if ($action == "create")    
+                            {{ csrf_field() }}
+                            {!! SemanticForm::text('name', 'Nama Satuan Kerja', old('name')) !!}
+                        	{!! SemanticForm::submit('Simpan') !!}
+                        @else
+                      	    {{ csrf_field() }}
+                            <input type="hidden" name="_method" value="PUT">
+                            <div class="field">
+                              	<label>Nama Satuan Kerja</label>
+                              	<input name="name" value="{{ (old('name')) ? old('name') : $satker->present('name') }}">
+                            </div>               
+                       	    {!! SemanticForm::submit('Simpan') !!}
+                        @endif   	
+                    </form>
                 </div>
             </div>
         </div>
     </section>		
-    </div>
+</div>
     
 @endsection
 
